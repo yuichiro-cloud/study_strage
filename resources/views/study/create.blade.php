c534bbf38bf9883c8edf463c39642349e15f4505
yuichiro-cloud <ghjhbc1@gmail.com>
2020-06-25 03:21:29 +0900

    createのviewを統一

--- resources/views/study/create.blade.php
+++ resources/views/study/create.blade.php
@@ -49,96 +49,18 @@
                         <div style="text-align:right;" class="form-group col" id="study_start_h">
                             <label for="exampleFormControlSelect1">Hour</label>
                             <select class="form-control" id="exampleFormControlSelect1">
-                            <option>00</option>
-                            <option>01</option>
-                            <option>02</option>
-                            <option>03</option>
-                            <option>04</option>
-                            <option>05</option>
-                            <option>06</option>
-                            <option>07</option>
-                            <option>08</option>
-                            <option>09</option>
-                            <option>10</option>
-                            <option>11</option>
-                            <option>12</option>
-                            <option>13</option>
-                            <option>14</option>
-                            <option>15</option>
-                            <option>16</option>
-                            <option>17</option>
-                            <option>18</option>
-                            <option>19</option>
-                            <option>20</option>
-                            <option>21</option>
-                            <option>22</option>
-                            <option>23</option>
+                                @foreach ($array_h as $s_h)
+                                    <option>{{$s_h}}</option>
+                                @endforeach
                             </select>
                             <label for="exampleFormControlSelect1">時</label>
                         </div>
                         <div style="text-align:right;" class="form-group col-md-6" id="study_start_m">
                             <label for="exampleFormControlSelect1">Minute</label>
                             <select class="form-control" id="exampleFormControlSelect1">
-                            <option>00</option>
-                            <option>01</option>
-                            <option>02</option>
-                            <option>03</option>
-                            <option>04</option>
-                            <option>05</option>
-                            <option>06</option>
-                            <option>07</option>
-                            <option>08</option>
-                            <option>09</option>
-                            <option>10</option>
-                            <option>11</option>
-                            <option>12</option>
-                            <option>13</option>
-                            <option>14</option>
-                            <option>15</option>
-                            <option>16</option>
-                            <option>17</option>
-                            <option>18</option>
-                            <option>19</option>
-                            <option>20</option>
-                            <option>21</option>
-                            <option>22</option>
-                            <option>23</option>
-                            <option>24</option>
-                            <option>25</option>
-                            <option>26</option>
-                            <option>27</option>
-                            <option>28</option>
-                            <option>29</option>
-                            <option>30</option>
-                            <option>31</option>
-                            <option>32</option>
-                            <option>33</option>
-                            <option>34</option>
-                            <option>35</option>
-                            <option>36</option>
-                            <option>37</option>
-                            <option>38</option>
-                            <option>39</option>
-                            <option>40</option>
-                            <option>41</option>
-                            <option>42</option>
-                            <option>43</option>
-                            <option>44</option>
-                            <option>45</option>
-                            <option>46</option>
-                            <option>47</option>
-                            <option>48</option>
-                            <option>49</option>
-                            <option>50</option>
-                            <option>51</option>
-                            <option>52</option>
-                            <option>53</option>
-                            <option>54</option>
-                            <option>55</option>
-                            <option>56</option>
-                            <option>57</option>
-                            <option>58</option>
-                            <option>59</option>
+                                @foreach ($array_m as $s_m)
+                                    <option>{{$s_m}}</option>
+                                @endforeach
                             </select>
                             <label for="exampleFormControlSelect1">分</label>
                         </div>
@@ -149,96 +71,18 @@
                         <div style="text-align:right;" class="form-group col" id="study_end_h">
                             <label for="exampleFormControlSelect1">Hour</label>
                             <select class="form-control" id="exampleFormControlSelect1">
-                            <option>00</option>
-                            <option>01</option>
-                            <option>02</option>
-                            <option>03</option>
-                            <option>04</option>
-                            <option>05</option>
-                            <option>06</option>
-                            <option>07</option>
-                            <option>08</option>
-                            <option>09</option>
-                            <option>10</option>
-                            <option>11</option>
-                            <option>12</option>
-                            <option>13</option>
-                            <option>14</option>
-                            <option>15</option>
-                            <option>16</option>
-                            <option>17</option>
-                            <option>18</option>
-                            <option>19</option>
-                            <option>20</option>
-                            <option>21</option>
-                            <option>22</option>
-                            <option>23</option>
+                                @foreach ($array_h as $e_h)
+                                    <option>{{$e_h}}</option>
+                                @endforeach
                             </select>
                             <label for="exampleFormControlSelect1">時</label>
                         </div>
                         <div style="text-align:right;" class="form-group col-md-6" id="study_end_m">
                             <label for="exampleFormControlSelect1">Minute</label>
                             <select class="form-control" id="exampleFormControlSelect1">
-                            <option>00</option>
-                            <option>01</option>
-                            <option>02</option>
-                            <option>03</option>
-                            <option>04</option>
-                            <option>05</option>
-                            <option>06</option>
-                            <option>07</option>
-                            <option>08</option>
-                            <option>09</option>
-                            <option>10</option>
-                            <option>11</option>
-                            <option>12</option>
-                            <option>13</option>
-                            <option>14</option>
-                            <option>15</option>
-                            <option>16</option>
-                            <option>17</option>
-                            <option>18</option>
-                            <option>19</option>
-                            <option>20</option>
-                            <option>21</option>
-                            <option>22</option>
-                            <option>23</option>
-                            <option>24</option>
-                            <option>25</option>
-                            <option>26</option>
-                            <option>27</option>
-                            <option>28</option>
-                            <option>29</option>
-                            <option>30</option>
-                            <option>31</option>
-                            <option>32</option>
-                            <option>33</option>
-                            <option>34</option>
-                            <option>35</option>
-                            <option>36</option>
-                            <option>37</option>
-                            <option>38</option>
-                            <option>39</option>
-                            <option>40</option>
-                            <option>41</option>
-                            <option>42</option>
-                            <option>43</option>
-                            <option>44</option>
-                            <option>45</option>
-                            <option>46</option>
-                            <option>47</option>
-                            <option>48</option>
-                            <option>49</option>
-                            <option>50</option>
-                            <option>51</option>
-                            <option>52</option>
-                            <option>53</option>
-                            <option>54</option>
-                            <option>55</option>
-                            <option>56</option>
-                            <option>57</option>
-                            <option>58</option>
-                            <option>59</option>
+                                @foreach ($array_m as $e_m)
+                                    <option>{{$e_m}}</option>
+                                @endforeach
                             </select>
                             <label for="exampleFormControlSelect1">分</label>
                         </div>
